8aadc3def90b186deb20f43e75d51eaad5e5e470
jasmitha reddy <25wh1a05z5@bvrithyderabad.edu.in>
2026-07-22 07:35:50 +0530

    conditionals and input

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-console.log("js connected");
-const appName="FocusHub";
-let totalTasks=0;
-console.log(appName);
-console.log(totalTasks);
 
 
+let studyHours=Number(prompt("how many hours did u study!"));
+if(studyHours>=4){
+    document.getElementById("display").innerHTML="amazing";
+}
+else if(studyHours>=3){
+    document.getElementById("display").innerHTML="keep going";
+}
+else{
+    document.getElementById("display").innerHTML="try studying a bit more!";
+}
